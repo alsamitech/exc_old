70ad86c2c2ccd9ee16600ba6a10923e39ac9dee8
Sami Alameddine <sala0599@stu.gusd.net>
2020-09-18 18:10:42 +0000

    Patched standard/exmath.h

--- standard/exmath.h
+++ standard/exmath.h
@@ -1,31 +1,31 @@
 int power(int baseNum, int powNum) {
   int result = 1;
-  for (int i = 0; i < powNum; i++;) {
+  for (int i = 0; i < powNum; i++) {
     result = result * baseNum;
   }
   return result; 
 }
 double powd(double baseDec, double powDec) {
   double resd = 1;
-  for(int di = 0; di < poweDec; di++) {
-    result = result*baseDec;
+  for(int di = 0; di < powDec; di++) {
+    resd = resd*baseDec;
   }
   return resd;
 }
 
 float powf(float basex, float powx) {
   double resf = 1;
-  for (int fi = 0; fi < powx; fi++:) {
+  for (int fi = 0; fi < powx; fi++) {
     resf = resf*basex;
   }
   return resf;
 }
 
-int ipw;
+int ipx;
 void ipw(int baseyu, int powyu) {
   int resl = 1;
-  for (int i = 0; i < powyu; i++;) {
+  for (int i = 0; i < powyu; i++) {
     resl = resl * baseyu;
   }
-  ipw = resl;
+  ipx = resl;
 }
